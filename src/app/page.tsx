9f33ac937bce7d4d52999ac94da76206ca0c8f1f
rohullah-ramish <rohullahramish9@gmail.com>
2024-10-15 23:49:06 +0500

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,10 +40,10 @@ export default function Home() {
         <div className="max-w-3xl mx-auto text-center">
           <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
             <span className="md:hidden">
-              Discover Who's Winning & Losing In SEO And Why
+              Discover Who&apos;s Winning & Losing In SEO And Why
             </span>
             <span className="hidden md:inline">
-              Discover Who's Winning and Losing in SEO and Why
+              Discover Who&apos;s Winning and Losing in SEO and Why
             </span>
           </h1>
           <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
@@ -56,7 +56,7 @@ export default function Home() {
                 href="/"
                 className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-sm font-semibold rounded-md text-white bg-[#4B42F4] md:py-4 md:text-md md:px-10"
               >
-                Create Landscape - It's Free
+                Create Landscape - It&apos;s Free
               </a>
             </div>
             <div className="mt-3 sm:mt-0 sm:ml-3">
